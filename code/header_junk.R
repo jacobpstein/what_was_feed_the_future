34735d3fa7be6a13d4ac90fd3cc476dd4cb1c58b
jacobpstein <jacobpstein@gmail.com>
2025-04-14 15:55:05 -0400

    just saving in case

---
+++ code/header_junk.R
@@ -0,0 +1,41 @@
+title: "What Was Feed the Future?"
+author: 
+  - name: Jacob Patterson-Stein
+lightbox: true
+format:
+  closeread-html:
+    embed-resources: true
+    cr-section:
+      layout: overlay-left
+      remove-header-space: true
+knitr:
+  opts_chunk: 
+    dev.args:
+      bg: transparent
+
+
+
+```{=html}
+<style>
+  .cr-section.overlay-left {
+    background-color: white;
+  }
+
+.cr-section.overlay-left .narrative-col .narrative {
+  font-size: 1.15em;
+  font: Gill Sans;
+  background-color: #3a79aa;
+    color: #f1f1f1;
+    padding: 0.6em 0.8em;
+  border-radius: 1;
+  backdrop-filter: blur(10px);
+}
+
+.cr-section.overlay-left .narrative-col .narrative a {
+  background-color: #3a79aa;
+    color: #f1f1f1;
+}
+
+
+</style>
+  ```
